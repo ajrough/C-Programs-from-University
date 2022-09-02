6c6a1361592eba0c77c4c9a344f0a8b5609d7cc3
Ajay ram <ajayramsaravanan4@gmail.com>
2022-09-02 18:03:34 +0530

    9-2-22 Update

---
+++ Week - 2/Control Structures/calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int choice;
+    cout<<"-----Calculator-----";
+    cout<<"1.Add"<<endl;
+    cout<<"2.Subtract"<<endl;
+    cout<<"3.Multiply"<<endl;
+    cout<<"4.Divide"<<endl;
+    cout<<"Input your Choice :"<<endl;
+    cin>>choice;
+    switch(choice){
+        case 1:
+            int a,b;
+            cout<<"Enter First number :"<<endl;
+            cin>>a;
+            cout<<"Enter Second number :"<<endl;
+            cin>>b;
+            cout<<"The ";
+    }
+    return 0;
+}
